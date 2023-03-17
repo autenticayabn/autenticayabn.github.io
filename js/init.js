0ed156eda5cb51ca61569f430167e9981f4bf47a
formulariiooyabn <formulariiooyabn@gmail.com>
2023-03-16 18:14:33 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+apiKey: "AIzaSyBUnTg0hq3zm-MBuTDbB7UXBo0D1U_vSZQ",
+    authDomain: "autenticayabn1.firebaseapp.com",
+    projectId: "autenticayabn1",
+    storageBucket: "autenticayabn1.appspot.com",
+    messagingSenderId: "141098224693",
+    appId: "1:141098224693:web:d463e6154bcfc7839b357c",
+    measurementId: "G-JJQR7FZ3HM"
+    });
